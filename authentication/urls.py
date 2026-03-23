79a31192deb66f70441021ba3a7988a78f2a2d4a
Kehinde Oluwasogo <77411180+KehindeOluwasogo-BC@users.noreply.github.com>
2026-03-23 18:10:04 +0000

    Replace TokenObtainPairView with CustomTokenObtainPairView

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
 from .views import (
     AuthRootView,
+    CustomTokenObtainPairView,
     RegisterView, 
     UserInfoView,
     RequestPasswordResetView,
@@ -25,7 +25,7 @@ from .views import (
 
 urlpatterns = [
     path('', AuthRootView.as_view(), name='auth_root'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('user/', UserInfoView.as_view(), name='user_info'),
